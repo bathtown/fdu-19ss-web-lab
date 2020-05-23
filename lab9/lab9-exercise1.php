22958f3322256b45c16ce71fef6dfb5b2f399791
hyf <hyf-0418@outlook.com>
2020-05-23 12:25:54 +0800

    finish lab9

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -1,17 +1,24 @@
 <html>
+
 <head>
-    <title>Exercise 1</title>
+  <title>Exercise 1</title>
 </head>
+
 <body>
-<h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
-<p>You can type regular HTML here and it will show up</p>
+  <h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+  <p>You can type regular HTML here and it will show up</p>
 
-<h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
-<?php
-//this is a php comment IN tags (will not appear)
-echo "This was output using PHP";
-echo "<br>"; //notice we must echo tags in php.
+  <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 
-?>
+  <?php
+  // set timezone
+  date_default_timezone_set("Asia/Shanghai");
+  $date = date("l, M dS, Y G:i:s");
+  echo "This page was generated: " . $date . "<hr/>";
+
+  $remaining = 365 - date("z") + date("L");
+  echo "There are " . $remaining . " days left in the year";
+  ?>
 </body>
+
 </html>
